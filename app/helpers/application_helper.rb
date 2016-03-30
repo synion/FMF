069bbefe262aac455f4a104d380e98e4ff5261a9
Synion <lach.szymon86@gmail.com>
2016-03-31 00:00:02 +0200

    LAst things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
 		new_object = f.object.send(association).klass.new
 		id = new_object.object_id
 		switchgears = f.fields_for(association, new_object, child_index: id) do |builder|
-			render(association.to_s.singularize + "_filed", f: builder )
+			render(association.to_s.singularize + "_field", f: builder )
 		end
-		link_to(name, '#', class: "add_switchgears", data: {id: id,  switchgears: fields.gsub("\n", "")})
+		link_to(name, '#', class: "add_switchgears", data: {id: id,  switchgears: switchgears.gsub("\n", "")})
 	end
 
 	
