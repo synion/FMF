5c735cc8e8f336bd70bd78c4fa462639001593ae
synion <lach.szymon86@gmail.com>
2017-02-14 19:59:23 +0100

    Repair tabs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
- 	has_many :switchgears, dependent: :destroy
+  has_many :switchgears, dependent: :destroy
   has_many :users
   belongs_to :admin
   accepts_nested_attributes_for :switchgears, reject_if: lambda { |a| a[:description].blank? }, allow_destroy: true
--- app/models/superadmin.rb
+++ app/models/superadmin.rb
@@ -2,5 +2,5 @@ class Superadmin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
-	has_many :admins
+  has_many :admins
 end
