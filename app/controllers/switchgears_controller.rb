8e099d7293581d921a9ebd1ea3f2bbfe45a2dca9
synion <lach.szymon86@gmail.com>
2017-02-14 19:17:40 +0100

    Repair tabs

--- app/controllers/switchgears_controller.rb
+++ app/controllers/switchgears_controller.rb
@@ -1,17 +1,17 @@
 class SwitchgearsController < ApplicationController
 
-	def show
-		@switchgear = Switchgear.find(params[:id])
-		@otdfs = @switchgear.otdfs.all
-		@project = params[:project_id]
-	end
+  def show
+    @switchgear = Switchgear.find(params[:id])
+    @otdfs = @switchgear.otdfs.all
+    @project = params[:project_id]
+  end
 
-	def destroy
-		project = Project.find(params[:project_id])
-		@switchgear = project.switchgears.find(params[:id])
-		@switchgear.destroy
-		respond_to do |format|
-			format.html { redirect_to project_url(@project), notice: 'Otdf was successfully destroyed.' }
-		end
-	end
+  def destroy
+    project = Project.find(params[:project_id])
+    @switchgear = project.switchgears.find(params[:id])
+    @switchgear.destroy
+    respond_to do |format|
+      format.html { redirect_to project_url(@project), notice: 'Otdf was successfully destroyed.' }
+    end
+  end
 end
